a55fa84e4213b871922166ff072e084a91a7831b
hclibraries <113636312+hclibraries@users.noreply.github.com>
2026-08-05 13:46:58 -0400

    Update new_items_cataloged.sql

--- queries/new_items_cataloged.sql
+++ queries/new_items_cataloged.sql
@@ -1,5 +1,5 @@
---metadb:function items_cataloged_date_range
-CREATE OR REPLACE FUNCTION items_cataloged_date_range (
+--metadb:function new_items_cataloged
+CREATE OR REPLACE FUNCTION new_cataloged (
 	start_date date DEFAULT '2000-01-01',
 	end_date date DEFAULT '2050-01-01')
 RETURNS TABLE(
